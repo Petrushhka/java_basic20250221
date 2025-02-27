a52ba58eb4f8362dfd103cafd9da2240e6c1e810
hajoon <stravinskky@naver.com>
2025-02-27 18:58:31 +0900

    Day05 - 상속

---
+++ src/basic/oop/inherit/Main.java
@@ -0,0 +1,12 @@
+package basic.oop.inherit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Warrior w1 = new Warrior("타락파워전사");
+//        w1.nickName = "전사짱짱맨";
+
+        w1.showStatus();
+
+    }
+}
